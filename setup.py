821be7d94b90a1ceb7471ef2118f6f2b42ae79f7
David Gu <david.gurquiza@gmail.com>
2013-08-04 00:53:57 -0500

    Now on pypi, pip install vinepy :old_man:

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'vinepy',
+  version = '0.5',
+  description = 'Python wrapper for the Vine Private API',
+  license = 'MIT',
+  author = 'David Gomez Urquiza',
+  author_email = 'david.gurquiza@gmail.com',
+  install_requires = ['requests>=1.2.3'],
+  url = 'https://github.com/davoclavo/vinepy',
+  keywords = 'vine library api wrapper',
+  packages = find_packages(),
+)
