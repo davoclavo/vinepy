3b781a86bfc4e3ff4ee8179e4a4528193f319d44
David Gu <david.gurquiza@gmail.com>
2015-02-16 16:45:09 -0500

    Version 0.6.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vinepy',
-    version='0.6.2',
+    version='0.6.3',
     description='Python wrapper for the Vine Private API',
     license='MIT',
     author='David Gomez Urquiza',
